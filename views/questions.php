178a616aa71e79f91db6ff6f7770cbf807405f93
bashet <a_bashet@yahoo.com>
2014-07-20 00:24:00 +0100

    update some text

--- views/questions.php
+++ views/questions.php
@@ -1,7 +1,7 @@
 <div id="questions-container">
 
     <form class="form-horizontal" role="form" id="save_question" method="post" action="{{ url:site }}survey/save_question">
-        <legend>Add new question</legend>
+        <legend>Question Text</legend>
         <div class="form-group">
             <label for="question_text1" class="col-sm-2 control-label">Question text for 1st person</label>
             <div class="col-sm-10">
@@ -16,7 +16,7 @@
             </div>
         </div>
 
-        <legend>Add answer options</legend>
+        <legend>Answer Options</legend>
         <div id="question_options">
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
@@ -24,7 +24,7 @@
                 </div>
             </div>
         </div>
-        <legend>Save changes</legend>
+        <legend>Save Changes</legend>
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
                 <button type="submit" class="btn btn-inverse">Save</button>
