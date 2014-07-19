cfeb8ffcff78822041d5dec4501047e31dcac0ec
bashet <a_bashet@yahoo.com>
2014-07-20 00:20:39 +0100

    implement tinymce editor done

--- js/question.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    $('#question_text').wysiwyg();
-});
--- views/questions.php
+++ views/questions.php
@@ -1,12 +1,18 @@
 <div id="questions-container">
 
-    <form class="form-horizontal" role="form">
+    <form class="form-horizontal" role="form" id="save_question" method="post" action="{{ url:site }}survey/save_question">
         <legend>Add new question</legend>
         <div class="form-group">
-            <label for="question_text" class="col-sm-2 control-label">Question Text</label>
+            <label for="question_text1" class="col-sm-2 control-label">Question text for 1st person</label>
             <div class="col-sm-10">
-                <!--<textarea class="form-control" id="question_text" name="question_text"></textarea>-->
-                <input type="text" class="form-control" id="question_text" name="question_text">
+                <textarea class="form-control" id="question_text1" name="question_text1"></textarea>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="question_text2" class="col-sm-2 control-label">Question text for 3rd person</label>
+            <div class="col-sm-10">
+                <textarea class="form-control" id="question_text2" name="question_text2"></textarea>
             </div>
         </div>
 
