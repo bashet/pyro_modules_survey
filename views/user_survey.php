dff89e1f67d802006e9e258a699b93a69c948492
bashet <a_bashet@yahoo.com>
2014-07-26 23:05:44 +0100

    just to check if I got the survey data

--- views/user_survey.php
+++ views/user_survey.php
@@ -2,6 +2,7 @@
     <?php
     if(($total_evaluators) && $total_evaluators >= 3){
         echo 'here is now showing questions to be answered';
+        var_dump($survey);
     }else{
         echo    '<h2>You have not nominated enough evaluators</h2>
                 <p>Once you have nominated at least 3 evaluators you will be able to complete the questions</p>';
