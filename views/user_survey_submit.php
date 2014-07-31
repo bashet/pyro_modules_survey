46f52ee1e18dbbfba7f8cae074c0709645523b09
bashet <a_bashet@yahoo.com>
2014-07-31 15:52:32 +0100

    show check in confirmation

--- views/user_survey_submit.php
+++ views/user_survey_submit.php
@@ -1,8 +1,9 @@
 <div id="user_survey_submit-container">
     <center>
         <div class="row">
-            <i class="fa fa-check fa-5x"></i>
+
             <div class="col-lg-8">
+                <i class="fa fa-check fa-5x"></i>
                 <h2>Your responses have now been submitted</h2>
 
                 <p>Once your evaluators have submitted their responses you will be able to generate your report.</p>
