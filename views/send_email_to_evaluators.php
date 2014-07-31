2905e522777cf16adacadaf6805c8d8cd0c3b844
bashet <a_bashet@yahoo.com>
2014-08-01 00:30:37 +0100

    apply class row

--- views/send_email_to_evaluators.php
+++ views/send_email_to_evaluators.php
@@ -3,10 +3,13 @@
     <p>This is some example text we may add here. But at this moment I don't have idea what to write. I will ask Mark to
         provide the appropriate text for this area. I hope this will look good as am writing many thinks which is probably cowshit.
         Alright, enough let's see how does it look like.. </p>
-    <form>
-        <textarea id="email_body" name="email_body" class="form-control"></textarea>
-    </form>
-    <div style="float: right">
+    <div class="row">
+        <form>
+            <textarea id="email_body" name="email_body" class="form-control"></textarea>
+        </form>
+    </div>
+
+    <div class="row" style="float: right">
         <button class="btn btn-primary" id="btn_send_email">Send Email <i class="fa fa-paper-plane"></i></button>
     </div>
 </div>
