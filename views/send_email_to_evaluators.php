fe135e6bcf2c28de93a17ee1dbf3a5850e576f33
bashet <a_bashet@yahoo.com>
2014-08-01 00:27:07 +0100

    add a form to allow to type email body

--- views/send_email_to_evaluators.php
+++ views/send_email_to_evaluators.php
@@ -1,3 +1,9 @@
 <div id="send_email_to_evaluators-container">
-
+    <h2>Send email notification to evaluators</h2>
+    <p>This is some example text we may add here. But at this moment I don't have idea what to write. I will ask Mark to
+        provide the appropriate text for this area. I hope this will look good as am writing many thinks which is probably cowshit.
+        Alright, enough let's see how does it look like.. </p>
+    <form>
+        <textarea id="email_body" name="email_body" class="form-control"></textarea>
+    </form>
 </div>
