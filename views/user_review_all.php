57d9432d1b1ea7ee262fdeaf7600533297af41ad
bashet <a_bashet@yahoo.com>
2014-08-01 00:12:59 +0100

    do not allow to click if not done all

--- views/user_review_all.php
+++ views/user_review_all.php
@@ -58,7 +58,7 @@
     </table>
     <?php if(! $ex_ans->submitted){?>
     <div style="float: right">
-        <button class="btn btn-primary" id="submit_answer">Submit <span class="glyphicon glyphicon-circle-arrow-right"></span></button>
+        <button class="btn btn-primary" id="submit_answer" <?php echo (! $ex_ans->finished)?'disabled':'';?>>Submit <span class="glyphicon glyphicon-circle-arrow-right"></span></button>
     </div>
     <?php }?>
 
