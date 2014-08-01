d10611e91c94ead396b4ab60a947ce58ccf4b8a2
bashet <a_bashet@yahoo.com>
2014-08-01 01:21:46 +0100

    set pre text for email body

--- views/send_email_to_evaluators.php
+++ views/send_email_to_evaluators.php
@@ -5,7 +5,19 @@
         Alright, enough let's see how does it look like.. </p>
     <div class="row">
         <form>
-            <textarea id="email_body" name="email_body" class="form-control"></textarea>
+            <textarea id="email_body" name="email_body" class="form-control">
+                <p>Dear {{evaluator_name}}</p>
+
+                <p>I would be very grateful if you could complete the National College Modular Curriculum online 360 diagnostic leadership for me.  This important process is part of my professional development as a school leader and will help shape my learning within the modular curriclulum. A number of other people will also be rating me on these important competencies. Please could you ensure that you have completed this process by {specify date DD/MM/YYY}.</p>
+
+                <p>The link to provide your ratings is:</p>
+
+                <p>{{ url:site }}</p>
+
+                <p>Regards
+                <br>
+                {{user_name}}</p>
+            </textarea>
         </form>
     </div>
 
