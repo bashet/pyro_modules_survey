54c666b0dbe564cdfc209ae038f5b281fb37873c
bashet <a_bashet@yahoo.com>
2014-08-03 22:32:43 +0100

    make home page for evaluators

--- views/evaluator_response.php
+++ views/evaluator_response.php
@@ -1,3 +1,23 @@
 <div id="evaluator_response-container">
-    question is coming here
+
+    <h2>Hello Dr. Abdul Bashet</h2>
+
+    <p>Welcome to the National College 360 degree leadership diagnostic</p>
+
+    <p>This diagnostic is designed to get feedback on this persons strengths and areas for development in a number of competencies related to leadership.
+        This feedback will be from you and from a selection of other raters. The diagnostic will help identify what
+        level they are working at for each of nineteen competencies. These are grouped into three clusters with specific competencies within each cluster.</p>
+
+    <p>As you progress through the diagnostic, you will be asked to consider the information given for each competency - particularly the descriptions of the behaviours likely at each of four levels for that competency - and select the level you feel that they are at.</p>
+
+    <p><strong>Guide on levels</strong></p>
+
+    <p>Each competency has four levels and you need to decide to what extent each competency is a strength or an area for development.
+        The default level for leaders who are solid performers is level three across all nineteen competencies.
+        It is expected that they will have a range levels across the different competencies.</p>
+
+    <p>If you want to find out more about using this diagnostic, please follow the 'View diagnostic guidance' link below.</p>
+
+    <a href="#" class="btn btn-info">View diagnostic guidance</a>
+
 </div>
