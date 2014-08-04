535dad89aff688a496011e3c8ca247c77579fce4
bashet <a_bashet@yahoo.com>
2014-08-04 15:34:01 +0100

    showing answer label in review all page

--- views/user_review_all.php
+++ views/user_review_all.php
@@ -26,19 +26,20 @@
         $i = 1;
         if($questions){
             foreach($questions as $q){
+                $answer = get_answers_by_q_id($q->id);
                 $q_id = $q->id;
                 echo '<tr>';
                 echo '<td>'.$i.'</td>';
                 echo '<td>'.$q->text1.'</td>';
                 if(isset($my_answer->$q_id)){
                     if($my_answer->$q_id == 1){
-                        echo '<td><strong>Level 1:</strong>&NonBreakingSpace;Requires development</td>';
+                        echo '<td><strong>Level 1:</strong>&NonBreakingSpace;'.$answer->option_1_label.'</td>';
                     }elseif($my_answer->$q_id == 2){
-                        echo '<td><strong>Level 2:</strong>&NonBreakingSpace;Emergent</td>';
+                        echo '<td><strong>Level 2:</strong>&NonBreakingSpace;'.$answer->option_2_label.'</td>';
                     }elseif($my_answer->$q_id == 3){
-                        echo '<td><strong>Level 3:</strong>&NonBreakingSpace;Effective</td>';
+                        echo '<td><strong>Level 3:</strong>&NonBreakingSpace;'.$answer->option_3_label.'</td>';
                     }elseif($my_answer->$q_id == 4){
-                        echo '<td><strong>Level 4:</strong>&NonBreakingSpace;Strength</td>';
+                        echo '<td><strong>Level 4:</strong>&NonBreakingSpace;'.$answer->option_4_label.'</td>';
                     }else{
                         echo '<td></td>';
                     }
