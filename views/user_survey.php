81a3306594fc3d040b2a18273afc05a1bbbc65b4
bashet <a_bashet@yahoo.com>
2014-08-07 18:49:46 +0100

    do not show the question if the current report is ready to view

--- views/user_survey.php
+++ views/user_survey.php
@@ -1,6 +1,6 @@
 <div id="user_survey-container">
     <?php
-    if(($total_evaluators) && $total_evaluators >= 3){
+    if(($total_evaluators) && ($total_evaluators >= 3) && ( ! $attempt->report_ready)){
         if($questions){
             $i = 1;
             //var_dump($my_answer);
