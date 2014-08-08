b692ad9d40cfb454ad7fef3a6e67bf80ac416065
bashet <a_bashet@yahoo.com>
2014-08-08 20:33:04 +0100

    delete logo from module

--- controllers/survey.php
+++ controllers/survey.php
@@ -1122,15 +1122,7 @@ class Survey extends Public_Controller {
         $data['base_url']   = $this->config->base_url();
         $data['attempt_id'] = $attempt_id;
 
-
         $this->load->view('pdf', $data);
-
-        /*
-        $this->load->library('pdf');
-        $this->pdf->load_view('pdf_report', $data);
-        $this->pdf->render();
-        $this->pdf->stream($attempt_id.".pdf", array('Attachment'=>0));
-        */
     }
 
     public function history(){
