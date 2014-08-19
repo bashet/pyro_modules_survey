8c97a7097bc0c7dbef894f5e2be60e504b3629b7
bashet <a_bashet@yahoo.com>
2014-08-19 21:18:17 +0100

    update text in organisation page

--- views/clients.php
+++ views/clients.php
@@ -82,7 +82,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title" id="update_clientsLabel">Update organisations information</h4>
+                    <h4 class="modal-title" id="update_clientsLabel">Update organisation information</h4>
                 </div>
 
                 <div class="modal-body">
