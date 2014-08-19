40cdb81595eaefbc1bf9a7c70c63b809cc1b2bf8
bashet <a_bashet@yahoo.com>
2014-08-19 21:23:00 +0100

    update category information

--- views/question_categories.php
+++ views/question_categories.php
@@ -41,7 +41,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title" id="update_question_categoriesLabel">Update question category information</h4>
+                    <h4 class="modal-title" id="update_question_categoriesLabel">Update category information</h4>
                 </div>
 
                 <div class="modal-body">
