193d922b23c7b54c20aa97ee3d57ee0941808e40
bashet <a_bashet@yahoo.com>
2014-09-04 22:40:25 +0100

    full height pdf load

--- views/report_viewer.php
+++ views/report_viewer.php
@@ -1,4 +1,6 @@
-<!--<iframe width="100%" src="--><?php //echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?><!--"-->
-<!--            height="1500px" frameborder="0" ></iframe>-->
+<!--<iframe width="100%" height="100%" src="--><?php //echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?><!--"-->
+<!--            frameborder="0" ></iframe>-->
 
-<embed width="100%" height="1500px" src="<?php echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?>">
+<div id="content">
+    <iframe width="100%" height="100%" frameborder="0" src="<?php echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?>" />
+</div>
