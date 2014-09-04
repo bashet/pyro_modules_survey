09a5bdaf27b23a4fc9bf0afa1f23aedbe1203603
bashet <a_bashet@yahoo.com>
2014-09-04 22:28:48 +0100

    load pdf with embd

--- views/report_viewer.php
+++ views/report_viewer.php
@@ -1,2 +1,4 @@
-<iframe width="100%" src="<?php echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?>"
-            height="1500px" frameborder="0" ></iframe>
+<!--<iframe width="100%" src="--><?php //echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?><!--"-->
+<!--            height="1500px" frameborder="0" ></iframe>-->
+
+<embed width="100%" height="1500px" src="<?php echo $base_url . 'index.php/survey/view_report/'. $attempt_id ;?>">
