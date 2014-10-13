3350895a25831f78bb0ff30dc9286da41b95ee97
bashet <a_bashet@yahoo.com>
2014-10-14 00:09:35 +0100

    trying to implement ajax source

--- views/manage_users.php
+++ views/manage_users.php
@@ -67,16 +67,7 @@
         </tr>
         </thead>
 
-        <tfoot>
-        <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Extn.</th>
-            <th>Start date</th>
-            <th>Salary</th>
-        </tr>
-        </tfoot>
+
     </table>
 
 
