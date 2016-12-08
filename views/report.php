0b972f0fa0ceaaa75c05465795c4fccf5d9a5b2f
Abdul Bashet <a_bashet@yahoo.com>
2016-12-08 15:49:32 +0000

    update report download page

--- views/report.php
+++ views/report.php
@@ -2,27 +2,16 @@
     <?php
     if($all_submitted){?>
 
-        <h2>Your report</h2>
-        <p>Your report is now available.</p>
+        <h2 class="center">Personal Feedback Report</h2>
+
         <center><a href="{{ url:site }}survey/view_report/<?php echo $attempt->id;?>" target="_blank">{{ theme:image file="DownloadPDF.gif" width="120px" }}</a></center>
 
     <?php }elseif($self_submit){ ?>
 
-        <h2>Generate report</h2>
-
-        <p>Not enough of your nominated evaluators have submitted their responses for you to be able to generate your report.
-            You need to have at least 3 evaluators to have submitted their responses.</p>
-
-        <p>You can select the 'Evaluator' link above to find out the status of all of your evaluators. On that page you can also
-            send an email to prompt any evaluators who may not yet have submitted their responses.
-        Before you generate you report you must first submit your responses.</p>
+        <div class="alert alert-danger"><h2>Report unavailable!</h2></div>
 
     <?php }else{ ?>
-        <h2>Generate report</h2>
-
-        <p>You haven't submitted your response.</p>
-
-        <p>Before you generate you report you must first submit your responses.</p>
+        <div class="alert alert-danger"><h2 class="center">Report unavailable!</h2></div>
     <?php } ?>
 
 
