ec0b3ef9bf470ea6e166243675767d2653bc2be6
Abdul Bashet <a_bashet@yahoo.com>
2017-01-28 16:32:09 +0000

    some column  not sortable

--- js/clients.js
+++ js/clients.js
@@ -3,7 +3,7 @@ $(function(){
     $(document).ready(function() {
         $('#all_clients').dataTable({
             "aoColumnDefs": [
-                { 'bSortable': false, 'aTargets': [ 2,4,5,6 ] }
+                { 'bSortable': false, 'aTargets': [ 2,3,4,5 ] }
             ]
         });
 
