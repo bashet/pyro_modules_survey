dcd150937a028b872b358b4eb83fce447ee8f16c
Abdul Bashet <a_bashet@yahoo.com>
2017-02-02 13:05:15 +0000

    update survey status logic

--- controllers/survey.php
+++ controllers/survey.php
@@ -2235,7 +2235,7 @@ class Survey extends Public_Controller {
     				DATE_FORMAT(from_unixtime(attempt.create_date), '%d-%m-%Y') as survey_started, 
     				IF(self.submitted=1, 'Yes', 'No') as self_submitted,  
     				concat(evaluator.submitted, ' out of ', evaluator.total) as evaluator_contribution, 
-    				IF(attempt.report_ready=1 AND evaluator.submitted=evaluator.total, 'Completed', IF(attempt.report_ready=1 AND evaluator.submitted!=evaluator.total, 'Closed', 'Pending')) as survey_status, 
+    				IF(attempt.report_ready=1 AND evaluator.submitted >= 3, 'Completed', IF(attempt.report_ready=1 AND evaluator.submitted!=evaluator.total, 'Closed', 'Pending')) as survey_status, 
     				IF(attempt.report_ready=1, DATE_FORMAT(from_unixtime(attempt.finished_date), '%d-%m-%Y'), '') as report_date
 			from default_users user
 			join default_profiles profile
