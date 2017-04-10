ab3cee214034abeb25287873e6dbc455a8471fea
Abdul Bashet <a_bashet@yahoo.com>
2017-04-10 17:40:25 +0100

    try to work with user response page

--- views/user_survey.php
+++ views/user_survey.php
@@ -33,19 +33,40 @@
                                 echo '</div>';
 
                                 echo '<table class="table table-bordered">';
-                                echo '<tr>';
-                                $q_id = $q->id;
-                                echo '<td style="text-align:center" width="25%"><input answer type="radio" name="q_answer-'.$q->id.'" value="1" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 1)?'checked':'').'></td>';
-                                echo '<td style="text-align:center" width="25%"><input answer type="radio" name="q_answer-'.$q->id.'" value="2" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 2)?'checked':'').'></td>';
-                                echo '<td style="text-align:center" width="25%"><input answer type="radio" name="q_answer-'.$q->id.'" value="3" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 3)?'checked':'').'></td>';
-                                echo '<td style="text-align:center" width="25%"><input answer type="radio" name="q_answer-'.$q->id.'" value="4" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 4)?'checked':'').'></td>';
+	                            $q_id = $q->id;
+	                            $col1 = '<input answer type="radio" name="q_answer-'.$q->id.'" value="1" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 1)?'checked':'').'>';
+	                            $col2 = '<input answer type="radio" name="q_answer-'.$q->id.'" value="2" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 2)?'checked':'').'>';
+	                            $col3 = '<input answer type="radio" name="q_answer-'.$q->id.'" value="3" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 3)?'checked':'').'>';
+	                            $col4 = '<input answer type="radio" name="q_answer-'.$q->id.'" value="4" '.(isset($my_answer->$q_id) && ($my_answer->$q_id == 4)?'checked':'').'>';
+                                echo '<tr class="hidden-sm hidden-xs">';
+                                echo '<td style="text-align:center" width="25%">'.$col1.'</td>';
+                                echo '<td style="text-align:center" width="25%">'.$col2.'</td>';
+                                echo '<td style="text-align:center" width="25%">'.$col3.'</td>';
+                                echo '<td style="text-align:center" width="25%">'.$col4.'</td>';
                                 echo '</tr>';
-                                echo '<tr>';
+                                echo '<tr class="hidden-sm hidden-xs">';
                                 echo '<td><strong>'.$answer->option_1_label.'</strong>'.$answer->option_1.'</td>';
                                 echo '<td><strong>'.$answer->option_2_label.'</strong>'.$answer->option_2.'</td>';
                                 echo '<td><strong>'.$answer->option_3_label.'</strong>'.$answer->option_3.'</td>';
                                 echo '<td><strong>'.$answer->option_4_label.'</strong>'.$answer->option_4.'</td>';
                                 echo '</tr>';
+
+                                echo '<tr class="hidden-md hidden-lg">';
+	                            echo '<td style="text-align:center" width="25%">'.$col1.'</td>';
+	                            echo '<td><strong>'.$answer->option_1_label.'</strong>'.$answer->option_1.'</td>';
+	                            echo '</tr>';
+	                            echo '<tr class="hidden-md hidden-lg">';
+	                            echo '<td style="text-align:center" width="25%">'.$col2.'</td>';
+	                            echo '<td><strong>'.$answer->option_2_label.'</strong>'.$answer->option_2.'</td>';
+	                            echo '</tr>';
+	                            echo '<tr class="hidden-md hidden-lg">';
+	                            echo '<td style="text-align:center" width="25%">'.$col3.'</td>';
+	                            echo '<td><strong>'.$answer->option_3_label.'</strong>'.$answer->option_3.'</td>';
+	                            echo '</tr>';
+	                            echo '<tr class="hidden-md hidden-lg">';
+	                            echo '<td style="text-align:center" width="25%">'.$col4.'</td>';
+	                            echo '<td><strong>'.$answer->option_4_label.'</strong>'.$answer->option_4.'</td>';
+	                            echo '</tr>';
                                 echo '</table>';
 
                                 echo '<div class="q_button" style="float: right">';
